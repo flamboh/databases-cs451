18283556221055e7cb4ef8519ec5665d752814c2
Jack Lund <jlund3@uoregon.edu>
2025-10-16 11:54:37 -0700

    finished pages.py

--- lstore/page.py
+++ lstore/page.py
@@ -1,14 +1,22 @@
+PAGE_SIZE = 4096
+INT_SIZE = 8
+MAX_SLOTS = PAGE_SIZE // INT_SIZE
 
 class Page:
 
     def __init__(self):
         self.num_records = 0
-        self.data = bytearray(4096)
+        self.data = bytearray(PAGE_SIZE)
+        self.capacity = MAX_SLOTS
 
     def has_capacity(self):
-        pass
+        return self.num_records < MAX_SLOTS
 
     def write(self, value):
+        if not self.has_capacity():
+            return False
+        offset = self.num_records * INT_SIZE
+        self.data[offset:offset + INT_SIZE] = value.to_bytes(INT_SIZE, byteorder='little', signed=True)
         self.num_records += 1
-        pass
+        return self.num_records - 1
 
