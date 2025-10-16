cd77921cb23f813980f0921175cd500b0a22c1ef
Emily Mayer <emilymayer234@gmail.com>
2025-10-16 15:46:42 -0700

    index.py implementations

--- lstore/index.py
+++ lstore/index.py
@@ -14,25 +14,42 @@ class Index:
     """
 
     def locate(self, column, value):
-        pass
+        #see if column has index
+        if self.indices[column] is None:
+            return []
+        
+        #see if value exists in index
+        if value in self.indices[column]:
+            return [self.indices[column][value]]
+        else:
+            return []
 
     """
     # Returns the RIDs of all records with values in column "column" between "begin" and "end"
     """
 
     def locate_range(self, begin, end, column):
-        pass
+        #see if column has index
+        if self.indices[column] is None:
+            return []
+
+        result = []
+        for value, rid in self.indices[column].items():
+            if begin <= value <= end:
+                result.append(rid)
+        return result
 
     """
     # optional: Create index on specific column
     """
 
     def create_index(self, column_number):
-        pass
+        if self.indices[column_number] is None:
+            self.indices[column_number] = {}
 
     """
     # optional: Drop index of specific column
     """
 
     def drop_index(self, column_number):
-        pass
+        self.indices[column_number] = None
