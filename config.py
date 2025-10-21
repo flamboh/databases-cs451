a2bb9fd6913b4d234542d2e2cc4f28e7c07e6817
flamboh <oliver.boorstein@gmail.com>
2025-10-21 15:15:53 -0700

    add table constants to config

--- config.py
+++ config.py
@@ -6,3 +6,7 @@ class Config:
     int_size = 8
     max_slots = page_size // int_size
     byteorder = 'little'
+    indirection_column = 0
+    rid_column = 1
+    timestamp_column = 2
+    schema_encoding_column = 3
