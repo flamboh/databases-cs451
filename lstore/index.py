e83f4e95f4110cdf9b3903c9133865e8935d5d89
Emily Mayer <emilymayer234@gmail.com>
2025-10-23 13:55:52 -0700

    cleanup & corrections

--- lstore/index.py
+++ lstore/index.py
@@ -1,3 +1,5 @@
+from lstore.bplus import BPlusTree
+
 """
 A data strucutre holding indices for various columns of a table. Key column should be indexd by default, other columns can be indexed through this object. Indices are usually B-Trees, but other data structures can be used as well.
 """
@@ -10,44 +12,31 @@ class Index:
         self.table = table
 
     def insert(self, rid, key_value, column=None):
-        col_index = self.table.key if column is None else column
-        if self.indices[col_index] is None:
-            self.create_index(col_index)
-        # handle multiple RIDs per value
-        if key_value in self.indices[col_index]:
-            self.indices[col_index][key_value].append(rid)
-        else:
-            self.indices[col_index][key_value] = [rid]
+        col = self.table.key if column is None else column
+        tree = self.indices[col]
+        if tree is not None:
+            tree.insert(key_value, rid)
 
     """
     # returns the location of all records with the given value on column "column"
     """
 
     def locate(self, column, value):
-        #see if column has index
-        if self.indices[column] is None:
+        tree = self.indices[column
+                            ]
+        if tree is None:
             return []
-        
-        #see if value exists in index
-        if value in self.indices[column]:
-            return [self.indices[column][value]]
-        else:
-            return []
+        return tree.find(value)
 
     """
     # Returns the RIDs of all records with values in column "column" between "begin" and "end"
     """
 
     def locate_range(self, begin, end, column):
-        #see if column has index
-        if self.indices[column] is None:
+        tree = self.indices[column]
+        if tree is None:
             return []
-
-        result = []
-        for value, rid in self.indices[column].items():
-            if begin <= value <= end:
-                result.extend(rid) # handles multiple RIDs
-        return result
+        return tree.find_range(begin, end)
 
     """
     # optional: Create index on specific column
@@ -55,7 +44,7 @@ class Index:
 
     def create_index(self, column_number):
         if self.indices[column_number] is None:
-            self.indices[column_number] = {}
+            self.indices[column_number] = BPlusTree(order=32)
 
     """
     # optional: Drop index of specific column
@@ -69,5 +58,8 @@ class Index:
     """
     def lookup(self, key_value):
         col_index = self.table.key
-        rid = self.indices[col_index].get(key_value, [])
-        return rid[0] if rid else None
+        tree = self.indices[col_index]
+        if tree is None:
+            return None
+        rids = tree.find(key_value)
+        return rids[0] if rids else None
