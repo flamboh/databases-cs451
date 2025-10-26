ef8af24963fa54ff3c844b21ac2ddce4442ad43d
flamboh <oliver.boorstein@gmail.com>
2025-10-26 00:01:52 -0700

    update testing to use less records and more efficient randomization

--- lstore/tests/test_table.py
+++ lstore/tests/test_table.py
@@ -1,8 +1,7 @@
 import pytest
-
 from config import Config
 from lstore.table import Table
-from random import randint, seed
+from random import randint, seed, sample
 
 def test_table_add_record_prints_page_directory():
     table = Table("students", num_columns=5, key=0)
@@ -23,21 +22,16 @@ def test_random_key_insertions():
   table = Table("grades", num_columns=5, key=0)
   records = {}
 
-  number_of_records = 200000
+  number_of_records = 10000
   seed(3562901)
 
-  for i in range(0, number_of_records):
-      key = 92106429 + randint(0, number_of_records)
+  keys = sample(range(92106429, 92106429 + number_of_records * 3), number_of_records)
 
-      #skip duplicate keys
-      while key in records:
-          key = 92106429 + randint(0, number_of_records)
-
-      records[key] = [-1 for _ in range(Config.base_meta_columns)] + [key] + [randint(0, 20) for _ in range(table.num_columns - 1)]
-      table.insert_record(records[key])
-
+  for key in keys:
+      rec = [-1 for _ in range(Config.base_meta_columns)] + [key] + [randint(0, 20) for _ in range(table.num_columns - 1)]
+      records[key] = rec
+      table.insert_record(rec)
+  
   for i in range(number_of_records):
     record = table.get_record(i)
-    print(record)
     assert record[Config.rid_column] == i
-    print(record[Config.base_meta_columns:])
